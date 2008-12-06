1cd45f1b8371bb6bd91d0d4c2c308fb7a4ca0b86
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2008-12-06 13:39:38 -0800

    Got checkboxes, radios, and selects working.

--- jquery.pageState.js
+++ jquery.pageState.js
@@ -1,7 +1,16 @@
 // Page state is a jQuery plugin that stores the state of form inputs on a
 // page behind an anchor in the URL.
+
+// TODO:
+//   * DONE should register unchecked checkboxes and radios as not having a value set
+//   * DONE should work for selects
+//   * DONE should handle multiple inputs with the same name... somehow
+//   * add a delay so you don't make a billion requests
+//   * if form passed in as scoping input, bind its submit to submit the url instead
+//   * DONE don't write params to the URL if they have no value 
 (function($){
   $.pageState = function(options) {
+    console.log("DEBUG: Initializing pageState...");
     var options = $.extend({}, options);
     
     // Store the options as a var
@@ -10,22 +19,62 @@
     var state = {};
     
     if (typeof(options.only) == 'undefined') {
-      var inputs = $("input[type!='submit']");
+      var inputs = $(':input');
     } else {
-      var inputs = $(options.only).find("input[type!='submit']");
+      var inputs = $(options.only).find(':input');
     }
     
     // Store the name/values of all inputs as key/values in the data cache
+    var getParams = $.extend({}, 
+      queryString2Obj(window.location.search), 
+      queryString2Obj(window.location.hash)
+    );
     inputs.each(function() {
+      console.log("DEBUG: Working on input: ", this);
       var name = $(this).attr('name');
-      if (typeof(name) == 'undefined' || name == '') { return false; };
-      // Store it
-      state[name] = $(this).val();
       
-      // Bind changes to the input to update the data cache and the url
-      $(this).change(function() {
-        $.pageState.change($(this).attr('name'), $(this).val());
-      });
+      // Ignore unnamed inputs
+      if (typeof(name) == 'undefined' || name == '') { return false; };
+      
+      switch ($(this).attr('type')) {
+        case 'checkbox':
+          if (typeof(getParams[name]) == 'undefined') {
+            state[name] = getCheckboxVal(this);
+          } else {
+            state[name] = getParams[name];
+            setCheckboxVals(this, state[name]);
+          };
+          $(this).change(function() {
+            $.pageState.change($(this).attr('name'), getCheckboxVal(this));
+          });
+          break;
+        case 'radio':
+          if (typeof(getParams[name]) == 'undefined') {
+            state[name] = getRadioVal(this);
+          } else {
+            state[name] = getParams[name];
+            setRadioVal(this, state[name]);
+          };
+          $(this).change(function() {
+            $.pageState.change($(this).attr('name'), getRadioVal(this));
+          });
+          break;
+        default:
+          // Store it
+          // If the input is also a GET param in the URL, set its value to the
+          // URL value (URL is the king)
+          if (typeof(getParams[name]) == 'undefined') {
+            state[name] = $(this).val();
+          } else {
+            state[name] = getParams[name];
+            $(this).val(state[name]);
+          };
+
+          // Bind changes to the input to update the data cache and the url
+          $(this).change(function() {
+            $.pageState.change($(this).attr('name'), $(this).val());
+          });
+      }
     });
     
     // Create the pageState hash in the data cache
@@ -45,26 +94,78 @@
     // Grab the anchor portion of the URL and make it a hash
     var anchorObj = {};
     if (typeof(window.location.hash) != 'undefined') {
-      var anchorStr = window.location.hash.substr(
-        1,window.location.hash.length);
-      $.each(anchorStr.split('&'), function() {
-        if ($.trim(this) != '') {
-          anchorObj[this.split('=')[0]] = this.split('=')[1];
-        };
-      });
+      // var anchorStr = window.location.hash.substr(
+      //   1, window.location.hash.length);
+      // var anchorStr = window.location.hash.replace(/^#/, '');
+      anchorObj = queryString2Obj(window.location.hash);
+      // $.each(anchorStr.split('&'), function() {
+      //   if ($.trim(this) != '') {
+      //     anchorObj[this.split('=')[0]] = this.split('=')[1];
+      //   };
+      // });
     };
     
     // Extend the hash with the current page state
     anchorObj = $.extend(anchorObj, $.pageState.state);
     
     // Write the anchor portion back to the URL
-    var anchorStr = '#';
-    var anchorBits = [];
-    $.each(anchorObj, function(k,v) {
-      anchorBits.push(k + '=' + v);
+    var anchorStr = '#' + obj2QueryString(anchorObj);
+    if (anchorStr != '#') {
+      window.location.hash = anchorStr;
+    };
+  }
+  
+  // Convert a URL query string to an object literal
+  function queryString2Obj(str) {
+    str = str.replace(/^[\?#]+/, '');
+    var pieces = str.split('&');
+    var obj = {};
+    jQuery.each(pieces, function() {
+      var bits = this.split('=');
+      obj[bits[0]] = bits[1];
     });
-    anchorStr += anchorBits.join('&');
-    window.location.hash = anchorStr;
+    return obj;
+  }
+  
+  function obj2QueryString(obj) {
+    var pieces = [];
+    jQuery.each(obj, function(k,v) {
+      if (typeof(v) != 'undefined' && v != '') {
+        pieces.push(k+'='+v); 
+      };
+    });
+    return pieces.join('&');
+  }
+  
+  function getCheckboxVal(input) {
+    var name = $(input).attr('name');
+    return $.makeArray(
+      $(':checkbox[name='+name+']:checked').map(
+        function() {return $(this).val()}
+      )
+    );
+  }
+  
+  function getRadioVal(input) {
+    var name = $(input).attr('name');
+    return $('input[name='+name+']:checked').val();
+  }
+  
+  function setCheckboxVals(input, values) {
+    var name = $(input).attr('name');
+    $('input[name='+name+']').each(function() {
+      console.log('input: ', this);
+      if ($.inArray(values, $(this).val())) {
+        this.checked = true;
+      } else {
+        this.checked = false;
+      };
+    });
+  }
+  
+  function setRadioVal(input, value) {
+    var name = $(input).attr('name');
+    $('input[name='+name+'][value='+value+']').get(0).checked = true;
   }
   
   $.pageState.requestData = function() {
